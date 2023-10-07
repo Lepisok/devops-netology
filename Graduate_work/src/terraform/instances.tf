211ce420336aab86c900706c1a88697176cd866a
Lepisok <aleksandr_podkop@mail.ru>
2023-10-07 23:26:08 +0300

    end of the first stage

--- Graduate_work/src/terraform/instances.tf
+++ Graduate_work/src/terraform/instances.tf
@@ -1,14 +1,13 @@
-resource "yandex_compute_instance" "nodes" {
-  for_each    = local.nodes
-  name        = each.key
-  zone        = var.zone
-  hostname    = "${each.key}.lepis.cloud"
-  description = each.value["desc"]
+resource "yandex_compute_instance" "vm-1" {
+  name        = "vm-1"
+  hostname    = "vm-1"
+  zone        = "ru-central1-a"
+  description = "vm-1"
   platform_id = "standard-v2"
 
   resources {
-    cores     = each.value["cores"]
-    memory    = each.value["memory"]
+    cores     = 4
+    memory    = 4
   }
 
   boot_disk {
@@ -34,18 +33,73 @@ resource "yandex_compute_instance" "nodes" {
   }
 }
 
-locals {
+resource "yandex_compute_instance" "vm-2" {
+  name        = "vm-2"
+  hostname    = "vm-2"
+  zone        = "ru-central1-b"
+  description = "vm-2"
+  platform_id = "standard-v2"
 
-  nodes = {
-    masternode = {
-      "cores" = 2
-      "memory" = 4
-      "desc" = "ubuntu-vm-1"
+  resources {
+    cores     = 4
+    memory    = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.image_id}"    ## Ubuntu 20.04 LTS
+      type        = "network-nvme"
+      size        = "30"
     }
-    workernode1 = {
-      "cores" = 2
-      "memory" = 4
-      "desc" = "ubuntu-vm-2"
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-2.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data   = "${file("./user.txt")}"
+    description = "The file includes the users to be added to the VM"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "yandex_compute_instance" "vm-3" {
+  name        = "vm-3"
+  hostname    = "vm-3"
+  zone        = "ru-central1-c"
+  description = "vm-3"
+  platform_id = "standard-v2"
+
+  resources {
+    cores     = 4
+    memory    = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "${var.image_id}"    ## Ubuntu 20.04 LTS
+      type        = "network-nvme"
+      size        = "30"
     }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-3.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data   = "${file("./user.txt")}"
+    description = "The file includes the users to be added to the VM"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
